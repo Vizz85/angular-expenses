638679109c9d6f6c54b6671bf0f62bfe5af16fbc
Vizz85 <alessandro.vizin@t-online.de>
2020-09-28 20:17:55 +0200

    Add more components, Update main styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,34 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExpenseAddComponent } from './expenses/expense-add/expense-add.component';
+import { HeaderComponent } from './header/header.component';
+import { ExpensesListComponent } from './expenses/expenses-list/expenses-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExpenseAddComponent
+    ExpenseAddComponent,
+    HeaderComponent,
+    ExpensesListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/expenses/expenses-list/expenses-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface Transaction {
+  item: string;
+  cost: number;
+}
+
+@Component({
+  selector: 'app-expenses-list',
+  templateUrl: './expenses-list.component.html',
+  styleUrls: ['./expenses-list.component.scss']
+})
+export class ExpensesListComponent implements OnInit {
+  displayedColumns: string[] = ['item', 'cost'];
+  transactions: Transaction[] = [
+    {item: 'Beach ball', cost: 4},
+    {item: 'Towel', cost: 5},
+    {item: 'Frisbee', cost: 2},
+    {item: 'Sunscreen', cost: 4},
+    {item: 'Cooler', cost: 25},
+    {item: 'Swim suit', cost: 15},
+  ];
+
+  /** Gets the total cost of all transactions. */
+  getTotalCost() {
+    return this.transactions.map(t => t.cost).reduce((acc, value) => acc + value, 0);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
